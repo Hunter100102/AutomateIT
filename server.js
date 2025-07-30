658214482f82f960c2d2f45fb39b5ab9efa83355
Hunter100102 <spc.cody.hunter@gmail.com>
2025-07-29 23:17:43 -0400

    corrected server.js

--- server.js
+++ server.js
@@ -10,13 +10,11 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-// Enable CORS for all origins (replace * with your GitHub Pages URL if needed)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://automatingsolutions.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+// Enable CORS
+const corsOptions = {
+  origin: ['https://automatingsolutions.com', 'https://hunter100102.github.io'],
+  optionsSuccessStatus: 200
+};
 
 app.use(cors(corsOptions));
 
